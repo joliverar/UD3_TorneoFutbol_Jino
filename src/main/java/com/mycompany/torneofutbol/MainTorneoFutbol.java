250fb754da83a2aad0f3473e93bf4a294b974efd
joliverar <joliverar2401@educantabria.es>
2025-03-10 15:40:26 +0100

    Update MainTorneoFutbol.java
    Se cambio el mensaje de bienvenida

--- src/main/java/com/mycompany/torneofutbol/MainTorneoFutbol.java
+++ src/main/java/com/mycompany/torneofutbol/MainTorneoFutbol.java
@@ -5,7 +5,7 @@ public class MainTorneoFutbol {
 
     public static void main(String[] args) {
 
-        System.out.println("Accediendo a la base de datos del torneo de FP");
+        System.out.println("Hola, voy a mostrar los jugadores de los equipos");
         
         // Crear una instancia de la clase Equipo
         Equipo equipoMH = new Equipo("IES MH FC");
